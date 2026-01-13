534c256b2bd1ef9069bde70ea231bb5983994a0c
Adhish G <120158636+Adhishg704@users.noreply.github.com>
2026-01-13 21:36:59 +0530

    Initial commit

---
+++ src/main/java/com/nemo/booktagger/BooktaggerApplication.java
@@ -0,0 +1,13 @@
+package com.nemo.booktagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktaggerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nemo/booktagger/entity/User.java
@@ -0,0 +1,40 @@
+package com.nemo.booktagger.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
