33d1af77ef7d2ad82cb5fe589df76d73f0614441
Adhish G <120158636+Adhishg704@users.noreply.github.com>
2026-01-15 17:56:11 +0530

    Added book entity.

---
+++ src/main/java/com/nemo/booktagger/entity/Book.java
@@ -0,0 +1,53 @@
+package com.nemo.booktagger.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "books")
+@Setter
+@Getter
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "isbn")
+    private long isbn;
+
+    @Column(name = "year_published")
+    private String yearPublished;
+
+    @Column(name = "status")
+    private String status;
+
+    public Book(String title, String author, String description, long isbn, String yearPublished, String status) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.isbn = isbn;
+        this.yearPublished = yearPublished;
+        this.status = status;
+    }
+}
