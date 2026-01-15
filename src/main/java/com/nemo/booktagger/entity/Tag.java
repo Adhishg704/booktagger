1524ad46a2d4ca8147d30af1587c1f2576f4296e
Adhish G <120158636+Adhishg704@users.noreply.github.com>
2026-01-15 18:07:08 +0530

    Added Tag entity.

---
+++ src/main/java/com/nemo/booktagger/entity/Tag.java
@@ -0,0 +1,41 @@
+package com.nemo.booktagger.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Tag {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+
+    @Column(name = "tag_name")
+    private String tagName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Tag(String tagName, User user) {
+        this.tagName = tagName;
+        this.user = user;
+    }
+}
