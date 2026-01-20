5abe7efcbacab69bda7f12d609b03552872170d5
Adhish G <120158636+Adhishg704@users.noreply.github.com>
2026-01-20 18:22:44 +0530

    Added more methods to UserBookRepository to find count of users owning a book and userbooks published in a particular year.

--- src/main/java/com/nemo/booktagger/dao/UserBookRepository.java
+++ src/main/java/com/nemo/booktagger/dao/UserBookRepository.java
@@ -5,6 +5,8 @@ import com.nemo.booktagger.enums.ReadingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserBookRepository extends JpaRepository<UserBook, Integer> {
     long countByUser_Id(Integer userId);
@@ -14,4 +16,11 @@ public interface UserBookRepository extends JpaRepository<UserBook, Integer> {
     long countByUser_IdAndStatus(Integer userId, ReadingStatus status);
 
     long countByUser_IdAndStatusAndYearRead(Integer userId, ReadingStatus status, Integer year);
+
+    long countByBook_Id(Integer bookId);
+
+    List<UserBook> findByUser_IdAndBook_YearPublished(
+            Integer userId,
+            String yearPublished
+    );
 }
--- src/test/java/com/nemo/booktagger/dao/UserBookRepositoryTest.java
+++ src/test/java/com/nemo/booktagger/dao/UserBookRepositoryTest.java
@@ -1,12 +1,16 @@
 package com.nemo.booktagger.dao;
 
 import com.nemo.booktagger.base.BaseRepositoryTest;
+import com.nemo.booktagger.entity.Book;
+import com.nemo.booktagger.entity.UserBook;
 import com.nemo.booktagger.enums.ReadingStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -53,5 +57,23 @@ public class UserBookRepositoryTest extends BaseRepositoryTest {
                 userBookRepository.countByUser_IdAndStatusAndYearRead(testUser.getId(), ReadingStatus.READ, 2025),
                 "Unexpected number of user books read in 2025"
         );
+    }
+
+    @Test
+    public void testCountByBookId() {
+        Book firstBook = testBooks.getFirst();
+        assertEquals(
+                1,
+                userBookRepository.countByBook_Id(firstBook.getId()),
+                "Expected only one user to own book"
+        );
+    }
+
+    @Test
+    public void testFindByUserIdAndBookYearPublished() {
+        Book firstBook = testBooks.getFirst();
+        List<UserBook> userBooks = userBookRepository.findByUser_IdAndBook_YearPublished(testUser.getId(),
+                firstBook.getYearPublished());
+        assertEquals(10, userBooks.size(), "User should own all the books released in 2025");
     }
 }
